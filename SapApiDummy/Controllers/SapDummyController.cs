e7ecab3d69d8b496671cdb33b55a20cf2fb09b2a
Jose Martinez <stn_rules@hotmail.com>
2015-11-07 10:26:48 -0500

    SapDummy

---
+++ SapApiDummy/Controllers/SapDummyController.cs
@@ -0,0 +1,25 @@
+﻿using SapApiDummy.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace SapApiDummy.Controllers
+{
+    public class SapDummyController : ApiController
+    {
+        [HttpGet]
+        public dynamic existItem([FromUri] int productKey) {
+            return new { exist = true, available = 10 };
+        }
+
+        [HttpGet]
+        [HttpPost]
+        public dynamic sendPayment([FromBody] PaymentRQ payment )
+        {
+            return new { message ="transaction completed"};
+        }
+    }
+}
---
+++ SapApiDummy/Models/PaymentRQ.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SapApiDummy.Models
+{
+    public class PaymentRQ
+    {
+        public int pro1 { get; set; }
+        public String pro2 { get; set; }
+        public int pro3 { get; set; }
+        public String pro4 { get; set; }
+    }
+}
